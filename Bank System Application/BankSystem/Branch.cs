794bdcbf1231685856b363a8a30af5957e3f4483
Mai Mohamed <maim51084@gmail.com>
2023-05-17 23:18:23 +0300

    Add Branch Implementation

--- Bank System Application/BankSystem/Branch.cs
+++ Bank System Application/BankSystem/Branch.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +8,23 @@ namespace BankSystem
 {
     internal class Branch
     {
+        public string BranchNumber { get; set; }
+        private string Address { get; set; }
+        private List<CustomerModel> Customers { get; set; }
+        private List<Loan> Loans { get; set; }
+        public Branch(string branchNumber ,string address, CustomerModel customers, Loan loan)
+        {
+            this.BranchNumber = branchNumber;
+            this.Address = address;
+            this.Customers = new List<CustomerModel>();
+            this.Loans = new List<Loan>();
+        }
+
+        public void AddCustomer(CustomerModel customer) { Customers.Add(customer); }
+        public void RemoveCustomer(CustomerModel customer) { Customers.Remove(customer); }
+        public void AddLoan(Loan loan) { Loans.Add(loan); }    
+        public void RemoveLoan(Loan loan) { Loans.Remove(loan); }
+
     }
 }
+
