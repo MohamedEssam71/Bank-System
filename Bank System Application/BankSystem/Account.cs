1e2c9b0ac89d779fc54738b79a592966db73ab52
Maya Zain El-Din <102183224+MayaZayn@users.noreply.github.com>
2023-05-17 20:18:04 +0300

    Add Account implementation

--- Bank System Application/BankSystem/Account.cs
+++ Bank System Application/BankSystem/Account.cs
@@ -8,5 +8,14 @@ namespace BankSystem
 {
     internal class Account
     {
+        public string AccountNumber { get; set; }
+        public double Balance { get; set; }
+        public string Type { get; set; }
+        public Account(string accountNumber, double balance, string Type)
+        {
+            this.AccountNumber = accountNumber;
+            this.Balance = balance;
+            this.Type = Type;
+        }
     }
 }
