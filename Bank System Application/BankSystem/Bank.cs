07ba3a1677b2850cbe6082f8d806730b3d1f3366
Maya Zain El-Din <102183224+MayaZayn@users.noreply.github.com>
2023-05-17 20:18:57 +0300

    Add Bank implementation

--- Bank System Application/BankSystem/Bank.cs
+++ Bank System Application/BankSystem/Bank.cs
@@ -8,5 +8,18 @@ namespace BankSystem
 {
     internal class Bank
     {
+        private int Code { get; set; }
+        private string Name { get; set; }
+        private string Address { get; set; }
+        private List<Branch> branches { get; }
+        public Bank(int code, string name, string address) {
+            this.Code = code;
+            this.Name = name;
+            this.Address = address;
+            branches = new List<Branch>();
+        }
+
+        public void AddBranch(Branch branch) { branches.Add(branch); }
+        public void RemoveBranch(Branch branch) { branches.Remove(branch); }
     }
 }
